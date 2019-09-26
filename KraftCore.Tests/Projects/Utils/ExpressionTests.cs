7a9bc9e0fbd58d2a0366e830efbcf33b0ec94042
Alexandre Murari Junior <murarialex@outlook.com>
2019-09-25 23:31:53 -0300

    Added unit tests for the expression builder

---
+++ KraftCore.Tests/Projects/Utils/ExpressionTests.cs
@@ -0,0 +1,358 @@
+﻿namespace KraftCore.Tests.Projects.Utils
+{
+    using System;
+    using System.Linq;
+    using KraftCore.Utils.Expressions;
+    using Xunit;
+
+    // ReSharper disable InconsistentNaming
+
+    /// <summary>
+    /// Tests for the expression builder/extensions.
+    /// </summary>
+    public class ExpressionTests
+    {
+        /// <summary>
+        /// Asserts that an numeric equality binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Equality_Binary_Expression_Of_Numeric_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.Age), randomPerson.Age, ExpressionOperator.Equal);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.Age == randomPerson.Age);
+            Assert.DoesNotContain(result, t => t.Age != randomPerson.Age);
+        }
+
+        /// <summary>
+        /// Asserts that an numeric non-equality binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Non_Equality_Binary_Expression_Of_Numeric_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.Age), randomPerson.Age, ExpressionOperator.NotEqual);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.Age != randomPerson.Age);
+            Assert.DoesNotContain(result, t => t.Age == randomPerson.Age);
+        }
+
+        /// <summary>
+        /// Asserts that an numeric less than binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Less_Than_Binary_Expression_Of_Numeric_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.Age), randomPerson.Age, ExpressionOperator.LessThan);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.Age < randomPerson.Age);
+            Assert.DoesNotContain(result, t => t.Age >= randomPerson.Age);
+        }
+
+        /// <summary>
+        /// Asserts that an numeric less than or equal binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Less_Than_Or_Equal_Binary_Expression_Of_Numeric_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.Age), randomPerson.Age, ExpressionOperator.LessThanOrEqual);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.Age <= randomPerson.Age);
+            Assert.DoesNotContain(result, t => t.Age > randomPerson.Age);
+        }
+
+        /// <summary>
+        /// Asserts that an numeric greater than binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Greater_Than_Binary_Expression_Of_Numeric_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.Age), randomPerson.Age, ExpressionOperator.GreaterThan);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.Age > randomPerson.Age);
+            Assert.DoesNotContain(result, t => t.Age <= randomPerson.Age);
+        }
+
+        /// <summary>
+        /// Asserts that an numeric greater than or equal binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Greater_Than_Or_Equal_Binary_Expression_Of_Numeric_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.Age), randomPerson.Age, ExpressionOperator.GreaterThanOrEqual);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.Age >= randomPerson.Age);
+            Assert.DoesNotContain(result, t => t.Age < randomPerson.Age);
+        }
+
+        /// <summary>
+        /// Asserts that an string equality binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Equality_Binary_Expression_Of_String_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FirstName), randomPerson.FirstName, ExpressionOperator.Equal);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.FirstName == randomPerson.FirstName);
+            Assert.DoesNotContain(result, t => t.FirstName != randomPerson.FirstName);
+        }
+
+        /// <summary>
+        /// Asserts that an string non-equality binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Non_Equality_Binary_Expression_Of_String_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FirstName), randomPerson.FirstName, ExpressionOperator.NotEqual);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.FirstName != randomPerson.FirstName);
+            Assert.DoesNotContain(result, t => t.FirstName == randomPerson.FirstName);
+        }
+
+        /// <summary>
+        /// Asserts that an string contains binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Contains_Binary_Expression_Of_String_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FullName), randomPerson.FirstName, ExpressionOperator.Contains);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.FullName.Contains(randomPerson.FirstName, StringComparison.OrdinalIgnoreCase));
+            Assert.DoesNotContain(result, t => t.FullName.Contains(randomPerson.FirstName, StringComparison.OrdinalIgnoreCase) == false);
+        }
+
+        /// <summary>
+        /// Asserts that an string starts with binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Starts_With_Binary_Expression_Of_String_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FullName), randomPerson.FirstName, ExpressionOperator.StartsWith);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.FullName.StartsWith(randomPerson.FirstName, StringComparison.OrdinalIgnoreCase));
+            Assert.DoesNotContain(result, t => t.FullName.StartsWith(randomPerson.FirstName, StringComparison.OrdinalIgnoreCase) == false);
+        }
+
+        /// <summary>
+        /// Asserts that an string ends with binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Ends_With_Binary_Expression_Of_String_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FullName), randomPerson.LastName, ExpressionOperator.EndsWith);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.FullName.EndsWith(randomPerson.LastName, StringComparison.OrdinalIgnoreCase));
+            Assert.DoesNotContain(result, t => t.FullName.EndsWith(randomPerson.LastName, StringComparison.OrdinalIgnoreCase) == false);
+        }
+
+        /// <summary>
+        /// Asserts that an generic collection contains binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Contains_Binary_Expression_Of_Generic_Collection_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var randomFavNumber = TestUtilities.GetRandomItem(randomPerson.FavoriteNumbers);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FavoriteNumbers), randomFavNumber, ExpressionOperator.Contains);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.FavoriteNumbers.Contains(randomFavNumber));
+            Assert.DoesNotContain(result, t => t.FavoriteNumbers.Contains(randomFavNumber) == false);
+        }
+
+        /// <summary>
+        /// Asserts that an non-generic collection contains binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Contains_Binary_Expression_Of_Non_Generic_Collection_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var randomFavFruit = TestUtilities.GetRandomItem(randomPerson.FavoriteFruits.ToArray());
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FavoriteFruits), randomFavFruit, ExpressionOperator.Contains);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.FavoriteFruits.Contains(randomFavFruit));
+            Assert.DoesNotContain(result, t => t.FavoriteFruits.Contains(randomFavFruit) == false);
+        }
+
+        /// <summary>
+        /// Asserts that an array contains binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Contains_Binary_Expression_Of_Array_Collection_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var randomPerson = TestUtilities.GetRandomItem(persons);
+            var randomFavColor = TestUtilities.GetRandomItem(randomPerson.FavoriteColors);
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FavoriteColors), randomFavColor, ExpressionOperator.Contains);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => t.FavoriteColors.Contains(randomFavColor));
+            Assert.DoesNotContain(result, t => t.FavoriteColors.Contains(randomFavColor) == false);
+        }
+
+        /// <summary>
+        /// Asserts that an generic collection contains on value binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Contains_On_Value_Binary_Expression_Of_Generic_Collection_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var favNumbers = TestUtilities.GetRandomItems(persons.Select(t => t.FavoriteNumbers));
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FavoriteNumbers), favNumbers, ExpressionOperator.ContainsOnValue);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => favNumbers.Contains(t.FavoriteNumbers));
+            Assert.DoesNotContain(result, t => favNumbers.Contains(t.FavoriteNumbers) == false);
+        }
+
+        /// <summary>
+        /// Asserts that an non-generic collection contains on value binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Contains_On_Value_Binary_Expression_Of_Non_Generic_Collection_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var favFruits = TestUtilities.GetRandomItems(persons.Select(t => t.FavoriteFruits));
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FavoriteFruits), favFruits, ExpressionOperator.ContainsOnValue);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => favFruits.Contains(t.FavoriteFruits));
+            Assert.DoesNotContain(result, t => favFruits.Contains(t.FavoriteFruits) == false);
+        }
+
+        /// <summary>
+        /// Asserts that an array contains on value binary expression gives the correct result.
+        /// </summary>
+        [Fact]
+        public void Assert_Contains_On_Value_Binary_Expression_Of_Array_Collection_Values_Gives_Correct_Result()
+        {
+            // Arrange
+            var persons = TestUtilities.GetFakePersonCollection();
+            var favColors = TestUtilities.GetRandomItems(persons.Select(t => t.FavoriteColors));
+            var expression = ExpressionBuilder.CreateBinaryExpression<TestUtilities.Person>(nameof(TestUtilities.Person.FavoriteColors), favColors, ExpressionOperator.ContainsOnValue);
+
+            // Act
+            var result = persons.Where(expression.Compile()).ToList();
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.Contains(result, t => favColors.Contains(t.FavoriteColors));
+            Assert.DoesNotContain(result, t => favColors.Contains(t.FavoriteColors) == false);
+        }
+    }
+}
---
+++ KraftCore.Tests/Utils.cs
@@ -0,0 +1,149 @@
+﻿namespace KraftCore.Tests
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Bogus;
+
+    /// <summary>
+    /// Utilities for unit testing.
+    /// </summary>
+    internal static class TestUtilities
+    {
+        /// <summary>
+        /// The faker used to create objects with dummy data.
+        /// </summary>
+        private static readonly Faker Faker;
+
+        /// <summary>
+        /// The person faker used to generate fake <see cref="Person"/> objects.
+        /// </summary>
+        private static readonly Faker<Person> PersonFaker;
+
+        /// <summary>
+        /// The list of colors.
+        /// </summary>
+        private static readonly string[] Colors = 
+        {
+            "Blue", "Red", "Yellow", "Purple", "Cyan", "Black", "White", "Green", "Brown"
+        };
+
+        /// <summary>
+        /// The list of fruits.
+        /// </summary>
+        private static readonly string[] Fruits =
+        {
+            "apple", "banana", "orange", "strawberry", "kiwi", "passion-fruit", "blueberry"
+        };
+
+        /// <summary>
+        /// Initializes static members of the <see cref="TestUtilities"/> class.
+        /// </summary>
+        static TestUtilities()
+        {
+            Faker = new Faker();
+
+            PersonFaker = new Faker<Person>()
+                .StrictMode(true)
+                .RuleFor(t => t.FirstName, f => f.Name.FirstName())
+                .RuleFor(t => t.LastName, f => f.Name.LastName())
+                .RuleFor(t => t.FullName, (f, p) => string.Concat(p.FirstName, " ", p.LastName))
+                .RuleFor(t => t.DateOfBirth, f => f.Date.Past(100, DateTime.Now.AddYears(-25)))
+                .RuleFor(t => t.Age, (f, p) => DateTime.Now.Year - p.DateOfBirth.Year)
+                .RuleFor(t => t.FavoriteNumbers, f => f.Random.ListItems(Enumerable.Range(1, 5000).ToList(), 5))
+                .RuleFor(t => t.FavoriteColors, f => f.Random.ArrayElements(Colors, 3))
+                .RuleFor(t => t.FavoriteFruits, f => new ArrayList(f.Random.ArrayElements(Fruits, 2)));
+
+            PersonFaker.AssertConfigurationIsValid();
+        }
+
+        /// <summary>
+        /// Gets an collection of <see cref="Person"/> objects filled with fake data.
+        /// </summary>
+        /// <param name="count">
+        /// The number of entries to be generated.
+        /// </param>
+        /// <returns>
+        /// The collection <see cref="Person"/> objects.
+        /// </returns>
+        internal static List<Person> GetFakePersonCollection(int count = 50) => PersonFaker.Generate(count);
+
+        /// <summary>
+        /// Gets an random item from the provided collection of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="collection">
+        /// The collection of <typeparamref name="T"/>.
+        /// </param>
+        /// <typeparam name="T">
+        /// The type of the generic collection.
+        /// </typeparam>
+        /// <returns>
+        /// The random <typeparamref name="T"/> object.
+        /// </returns>
+        internal static T GetRandomItem<T>(IEnumerable<T> collection) => Faker.Random.CollectionItem(collection.ToList());
+
+        /// <summary>
+        /// Gets an random collection of items from the provided collection of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="collection">
+        /// The collection of <typeparamref name="T"/>.
+        /// </param>
+        /// <param name="count">
+        /// The number of elements to be returned.
+        /// </param>
+        /// <typeparam name="T">
+        /// The type of the generic collection.
+        /// </typeparam>
+        /// <returns>
+        /// The random collection of <typeparamref name="T"/>.
+        /// </returns>
+        internal static T[] GetRandomItems<T>(IEnumerable<T> collection, int count = 5) => Faker.Random.ArrayElements(collection.ToArray(), count);
+
+        /// <summary>
+        /// Represents an person.
+        /// </summary>
+        internal class Person
+        {
+            /// <summary>
+            /// Gets or sets the first name.
+            /// </summary>
+            public string FirstName { get; set; }
+
+            /// <summary>
+            /// Gets or sets the last name.
+            /// </summary>
+            public string LastName { get; set; }
+
+            /// <summary>
+            /// Gets or sets the full name.
+            /// </summary>
+            public string FullName { get; set; }
+
+            /// <summary>
+            /// Gets or sets the age.
+            /// </summary>
+            public int Age { get; set; }
+
+            /// <summary>
+            /// Gets or sets the date of birth.
+            /// </summary>
+            public DateTime DateOfBirth { get; set; }
+
+            /// <summary>
+            /// Gets or sets the favorite numbers.
+            /// </summary>
+            public ICollection<int> FavoriteNumbers { get; set; }
+
+            /// <summary>
+            /// Gets or sets the favorite colors.
+            /// </summary>
+            public string[] FavoriteColors { get; set; }
+
+            /// <summary>
+            /// Gets or sets the favorite fruits.
+            /// </summary>
+            public ArrayList FavoriteFruits { get; set; }
+        }
+    }
+}
