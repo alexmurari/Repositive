4c1328e8716e02a6093c8d1134b581a22bc98ecb
Alexandre Murari Junior <murarialex@outlook.com>
2019-09-27 08:29:52 -0300

    Added service interface; Modified projects descriptions;

---
+++ KraftCore.Domain/Contracts/Service/IGenericService.cs
@@ -0,0 +1,16 @@
+﻿namespace KraftCore.Domain.Contracts.Service
+{
+    using KraftCore.Domain.Contracts.Repository;
+
+    /// <summary>
+    /// Provides methods for service operations on instances of <typeparamref name="TEntity"/>.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity for which the service will operate.</typeparam>
+    public interface IGenericService<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Gets the repository.
+        /// </summary>
+        IGenericRepository<TEntity> Repository { get; }
+    }
+}
--- KraftCore.Service/GenericService.cs
+++ KraftCore.Service/GenericService.cs
@@ -1,6 +1,7 @@
 ﻿namespace KraftCore.Service
 {
     using KraftCore.Domain.Contracts.Repository;
+    using KraftCore.Domain.Contracts.Service;
 
     /// <summary>
     /// Base class for generic service implementations that operates on values of type <typeparamref name="TEntity"/>.
@@ -9,7 +10,7 @@
     /// <typeparam name="TEntity">
     /// The underlying type that this service operates on.
     /// </typeparam>
-    public abstract class GenericService<TEntity> where TEntity : class
+    public abstract class GenericService<TEntity> : IGenericService<TEntity> where TEntity : class
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="GenericService{TEntity}"/> class.
@@ -25,6 +26,6 @@
         /// <summary>
         /// Gets the repository.
         /// </summary>
-        protected IGenericRepository<TEntity> Repository { get; }
+        public IGenericRepository<TEntity> Repository { get; }
     }
 }
