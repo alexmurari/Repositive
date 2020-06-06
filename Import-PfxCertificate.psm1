0472316efda638f7d7c5483ce46b20d06fb89217
Alexandre Murari Junior <murarialex@outlook.com>
2020-06-05 21:36:49 -0300

    Update build scripts

--- Import-PfxCertificate.psm1
+++ Import-PfxCertificate.psm1
@@ -17,7 +17,7 @@ function Import-PfxCertificate {
 
 		$csp = New-Object System.Security.Cryptography.CspParameters;
 		$csp.KeyContainerName = $containerName;
-		$csp.Flags = [System.Security.Cryptography.CspProviderFlags]::UseMachineKeyStore -bor [System.Security.Cryptography.CspProviderFlags]::NoPrompt; # -bor is biwise or
+		$csp.Flags = [System.Security.Cryptography.CspProviderFlags]::UseMachineKeyStore -bor [System.Security.Cryptography.CspProviderFlags]::NoPrompt; # -bor is bitwise or
 		$csp.KeyNumber = [System.Security.Cryptography.KeyNumber]::Signature;
 
 		$rsa = New-Object System.Security.Cryptography.RSACryptoServiceProvider $csp;
